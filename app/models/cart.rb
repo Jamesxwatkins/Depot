f3c7d93846058b8dc17c973cf9d51f5a558fc9a6
James Watkins <jameswatkins@live.com>
2014-03-10 22:34:39 -0400

    fixed error inside product.rb causting 500 error

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,18 @@
 class Cart < ActiveRecord::Base
+	has_many :line_items, dependant: :destroy
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+	private
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items Present')
+			
+			return false
+
+		end
+	end
 end
