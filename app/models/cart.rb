6fecb94ba7cb5293782fac4370aa26556b509eda
James Watkins <jameswatkins@live.com>
2014-04-21 17:37:00 -0400

    standardized the line item creation and adding products to the carts. both now work with instances of the Product class

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,18 @@ class Cart < ActiveRecord::Base
 
 	#before_destroy :ensure_not_referenced_by_any_line_item
 
+
+
+	def add_product(product)
+		current_item = line_items.find_by(product_id: product.id)
+		if current_item
+			current_item.quantity += 1
+		else
+			current_item = line_items.build(product_id:product.id)
+		end
+		current_item
+	end
+
 	#private
 
 	#def ensure_not_referenced_by_any_line_item
@@ -15,14 +27,4 @@ class Cart < ActiveRecord::Base
 
 	#	end
 	#end
-
-	def add_product(product_id)
-		current_item = line_items.find_by(product_id:product_id)
-		if current_item
-			current_item.quantity += 1
-		else
-			current_item = line_items.build(product_id:product_id)
-		end
-		current_item
-	end
 end
